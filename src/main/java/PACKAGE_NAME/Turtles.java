077cb3e9169bb4f300a7733e92172fd64ca37548
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-07-29 00:21:01 -0400

    recursion all the way

---
+++ src/main/java/PACKAGE_NAME/Turtles.java
@@ -0,0 +1,59 @@
+package PACKAGE_NAME;
+
+import java.math.BigInteger;
+
+/**
+ * Created by kristofer on 7/14/20.
+ */
+public class Turtles {
+    public static long factorial(int n) {
+        if (n == 1) return 1;
+        return n * factorial(n-1);
+    }
+
+    // recursive implementation
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+    }
+
+    // non-recursive implementation
+    public static int gcd2(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
+    // Compute length of LCS for all subproblems.
+    public static String lcs(String x, String y) {
+        int m = x.length(), n = y.length();
+        int[][] opt = new int[m+1][n+1];
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+                if (x.charAt(i) == y.charAt(j)) {
+                    opt[i][j] = opt[i+1][j+1] + 1;
+                }
+                else {
+                    opt[i][j] = Math.max(opt[i+1][j], opt[i][j+1]);
+                }
+            }
+        }
+
+        // Recover LCS itself.
+        String lcs = "";
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (x.charAt(i) == y.charAt(j)) {
+                lcs += x.charAt(i);
+                i++;
+                j++;
+            }
+            else if (opt[i+1][j] >= opt[i][j+1]) i++;
+            else                                 j++;
+        }
+        return lcs;
+    }
+}
