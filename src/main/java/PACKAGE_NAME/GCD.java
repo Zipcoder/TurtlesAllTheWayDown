e7f984f709945a987539aff870ec91c3175abc77
jgiroso <jrgiroso@gmail.com>
2021-08-01 14:52:44 -0400

    GCD complete

--- src/main/java/PACKAGE_NAME/GCD.java
+++ src/main/java/PACKAGE_NAME/GCD.java
@@ -1,2 +1,19 @@
+package PACKAGE_NAME;
+
 public class GCD {
+
+    public static int gcd(int p, int q) {
+        if(q == 0) {
+            return p;
+        } else return gcd(q, p % q);
+    }
+
+    public static int gcd2(int p, int q) {
+        while(q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
 }
