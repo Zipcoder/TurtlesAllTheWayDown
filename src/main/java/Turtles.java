0df3e6629d2f2afee9165234c938dfdcf3e9a7b5
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-08-03 20:13:48 -0400

    factorial tests passed

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
 
-/**
- * Created by kristofer on 7/14/20.
- */
+
 public class Turtles {
-}
+        private static  final Turtles INSTANCE = new Turtles();
+
+        private Turtles(){};
+
+        public static Turtles getInstance(){
+            return INSTANCE;
+        }
+
+        public int factorial(int n) {
+            if (n == 1){
+                return 1;
+            }
+            return factorial(n-1) * n;
+        }
+
+        public
+    }
+
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,38 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+    Turtles turtles = Turtles.getInstance();
+
+    @Test
+    public void factorialTest(){
+        //given
+        int n = 3;
+        int expected = 6;
+
+        //when
+        int actual = turtles.factorial(n);
+
+        //then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void factorialTest2(){
+        //given
+        int n = 9;
+        int expected = 362880;
+
+        //when
+        int actual = turtles.factorial(n);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
 
 }
