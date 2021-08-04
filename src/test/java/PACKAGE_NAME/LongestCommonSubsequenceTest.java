baaf859163698806ab3ff172c81d622f286ffe65
MannyMb <emmanuelnefo98@gmail.com>
2021-08-03 22:53:02 -0400

    finn

---
+++ src/test/java/PACKAGE_NAME/LongestCommonSubsequenceTest.java
@@ -0,0 +1,32 @@
+package PACKAGE_NAME;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LongestCommonSubsequenceTest {
+    @Test
+    public void lcsTest1() {
+        // Given
+        String expectedString = "GGCAACG";
+
+        // When
+        String actualString = LongestCommonSubsequence.lcs("GGCACCACG","ACGGCGGATACG");
+
+        // Then
+        Assert.assertEquals(expectedString,actualString);
+
+    }
+
+    @Test
+    public void lcsTest2() {
+        // Given
+        String expectedString = "GGCAACG";
+
+        // When
+        String actualString = LongestCommonSubsequence.lcs("ACGGCGGATACG","GGCACCACG");
+
+        // Then
+        Assert.assertEquals(expectedString,actualString);
+
+    }
+}
